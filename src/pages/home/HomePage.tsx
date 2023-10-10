d6f0561f5cbeb65b4bcee986166005e7793407ba
Paulina <paulina2651997@gmail.com>
2023-10-10 18:24:21 +0200

    style homePage. add background bubble falg

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,25 @@
+import * as S from './StyleHomePage';
+import Background from '../../components/atoms/Background';
+
 export const HomePage = () => {
   return (
     <>
-      <div>
-        <p>Testy</p>
-      </div>
+      <Background />
+      <S.WrapperHome>
+        <S.WrapperOneWord>
+          <S.TittleOnly>only</S.TittleOnly>
+          <S.TittleOneWord>One Word</S.TittleOneWord>
+          <S.TittleaDay>a day</S.TittleaDay>
+        </S.WrapperOneWord>
+        <S.WrapperContainer>
+          <S.Tittle>Hello</S.Tittle>
+          <S.Description>It’s a pleasure to see you here</S.Description>
+          <S.Description>You don’t have much time?</S.Description>
+          <S.Description>Great! One word a day it’s enough!</S.Description>
+          <S.Description>Create your own database of words to learn.</S.Description>
+          <S.Button>Try It</S.Button>
+        </S.WrapperContainer>
+      </S.WrapperHome>
     </>
   );
 };
