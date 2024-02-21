d313157ba6815870e71860b02e2cab949223ed5a
Paulina <paulina2651997@gmail.com>
2024-02-21 14:41:42 +0100

    fixed and cleaning

--- src/pages/User/Settings/SettingsApp.tsx
+++ src/pages/User/Settings/SettingsApp.tsx
@@ -10,11 +10,21 @@ import { optionsCategory } from 'data/option/category_options';
 import { optionsLanguage } from 'data/option/language_options';
 import { WrapperSettings } from 'components/Shared/containers/WrapperSettings';
 import { UserSettingsContext } from 'utils/ContextSettingsUser';
+import { Controller, SubmitHandler, useForm, useFieldArray } from 'react-hook-form';
 
 export const Tittle = styled.p`
   ${font_settings(2.4, 'normal', 600)}
   margin: 5px auto;
 `;
+
+interface IForm {
+  selectedOptionLanguageWord: string;
+  selectedTranslateLanguageWord: string;
+  notifications: {
+    type: string;
+    time: string;
+  }[];
+}
 const SettingsApp = () => {
   const {
     defaultWordLanguage,
@@ -22,34 +32,64 @@ const SettingsApp = () => {
     defaultWordLanguageTranslate,
     setDefaultWordLanguageTranslate,
   } = useContext(UserSettingsContext);
-  const [selectedOptionLanguageWord, setSelectedOptionLanguageWord] = useState(defaultWordLanguage);
-  const [selectedTranslateLanguageWord, setSelectedTranslateLanguageWord] = useState(
-    defaultWordLanguageTranslate
-  );
+
+  const { handleSubmit, control, getValues } = useForm<IForm>({
+    defaultValues: {
+      selectedOptionLanguageWord: defaultWordLanguage,
+      selectedTranslateLanguageWord: defaultWordLanguageTranslate,
+    },
+  });
+
+  const { fields, remove, append } = useFieldArray({ control, name: 'notifications' });
+
+  const onSubmit: SubmitHandler<IForm> = data => {
+    setDefaultWordLanguage(data.selectedOptionLanguageWord);
+    setDefaultWordLanguageTranslate(data.selectedTranslateLanguageWord);
+    console.log(data);
+  };
 
   return (
     <WrapperSettings>
       <Tittle>Settings Word</Tittle>
       <div>
-        <form action='submit'>
-          <Select
-            id='select_GlobalSettingsApp_LanguageDefault'
-            $fontColorLabel='purpleDark'
-            labelValue='Select Basic Language Word'
-            options={optionsLanguage}
-            value={selectedOptionLanguageWord}
-            onChange={value => setSelectedOptionLanguageWord(value)}
-            $isLightTeam={true}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name='selectedOptionLanguageWord'
+            control={control}
+            render={({ field }) => (
+              <Select
+                id='select_GlobalSettingsApp_LanguageDefault'
+                $fontColorLabel='purpleDark'
+                labelValue='Select Basic Language Word'
+                options={optionsLanguage}
+                value={field.value}
+                onChange={field.onChange}
+                $isLightTeam={true}
+              />
+            )}
           />
-          <Select
-            id='select_GlobalSettingsApp_LanguageTranslate'
-            $fontColorLabel='purpleDark'
-            labelValue='Select Translate Language Word '
-            options={optionsLanguage}
-            value={selectedTranslateLanguageWord}
-            onChange={value => setSelectedTranslateLanguageWord(value)}
-            $isLightTeam={true}
+          <Controller
+            name='selectedTranslateLanguageWord'
+            control={control}
+            render={({ field }) => (
+              <Select
+                id='select_GlobalSettingsApp_LanguageTranslate'
+                $fontColorLabel='purpleDark'
+                labelValue='Select Translate Language Word '
+                options={optionsLanguage}
+                value={field.value}
+                onChange={field.onChange}
+                $isLightTeam={true}
+              />
+            )}
           />
+
+          {fields.map(el => (
+            <span key={el.time}>
+              {el.type} - {el.time}
+            </span>
+          ))}
+
           <Submit $isLightTeam={true} value='Apply' id='submit_GlobalSettingsApp' />
         </form>
       </div>
