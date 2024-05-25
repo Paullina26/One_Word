3ddb5a090c935c35c4b9c2faf58f06c0e1ca8e4f
Paulina Golenia <70859448+Paullina26@users.noreply.github.com>
2024-05-25 15:13:13 +0200

    tray

--- src/pages/User/Settings/Learning.tsx
+++ src/pages/User/Settings/Learning.tsx
@@ -1,5 +1,19 @@
+import Input from 'components/Shared/Form/Input';
+import { GlassWrapper } from 'components/Shared/containers/GlassWrapper';
+import styled from 'styled-components';
+
+const Wrapper = styled(GlassWrapper)`
+  margin: 20px auto;
+  height: 500px;
+  width: 500px;
+`;
+
 const LearningSettings = () => {
-  return <div>Learning Settings</div>;
+  return (
+    <Wrapper>
+      <div>Learning Settings</div>
+    </Wrapper>
+  );
 };
 
 export default LearningSettings;
