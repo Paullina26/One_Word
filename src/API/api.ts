1960548553e9d5548e9d0983d90d7737ad3d33ff
zofia janas <zoska91@wp.pl>
2024-06-10 19:26:39 +0200

    change api source

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080/api/';
-// export const API_BASE_URL = 'https://onewordserv.byst.re/api/';
+// export const API_BASE_URL = 'http://localhost:8080/api/';
+export const API_BASE_URL = 'https://onewordserv.byst.re/api/';
 //
 export const headers = {
   Accept: 'application.json',
