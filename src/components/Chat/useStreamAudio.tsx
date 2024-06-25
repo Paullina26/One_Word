882d6b01340d0ea06da5c4b5b09bcac59394bbcd
zofia janas <zoska91@wp.pl>
2024-06-25 12:15:48 +0200

    try fix

--- src/components/Chat/useStreamAudio.tsx
+++ src/components/Chat/useStreamAudio.tsx
@@ -35,6 +35,9 @@ export const useStreamAudio = ({
 
   useEffect(() => {
     getApiKey();
+    if (isSafariOrIOS()) {
+      warmupAudioContextForIOS();
+    }
   }, []);
 
   const getApiKey = async () => {
@@ -115,6 +118,47 @@ export const useStreamAudio = ({
     }
 
     streamAudio(reader);
+  };
+
+  const isSafariOrIOS = () => {
+    const userAgent = window.navigator.userAgent;
+    const isIOS = /iP(hone|od|ad)/.test(userAgent);
+    const isSafari = /^((?!chrome|android).)*safari/i.test(userAgent);
+    return isIOS || isSafari;
+  };
+
+  const warmupAudioContextForIOS = () => {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    if (window.AudioContext) {
+      window.audioContext = new window.AudioContext();
+    }
+
+    const fixAudioContext = function (e) {
+      if (window.audioContext) {
+        // Create empty buffer
+        var buffer = window.audioContext.createBuffer(1, 1, 22050);
+        var source = window.audioContext.createBufferSource();
+        source.buffer = buffer;
+        // Connect to output (speakers)
+        source.connect(window.audioContext.destination);
+        // Play sound
+        if (source.start) {
+          source.start(0);
+        } else if (source.play) {
+          source.play(0);
+        } else if (source.noteOn) {
+          source.noteOn(0);
+        }
+      }
+      // Remove events
+      document.removeEventListener('touchstart', fixAudioContext);
+      document.removeEventListener('touchend', fixAudioContext);
+    };
+
+    // iOS 6-8
+    document.addEventListener('touchstart', fixAudioContext);
+    // iOS 9+
+    document.addEventListener('touchend', fixAudioContext);
   };
 
   return { streamAudio, streamingAnswer };
