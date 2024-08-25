165dd5aba1c27f0cffcd49359625b849c05ea9cb
zofia janas <zoska91@wp.pl>
2024-08-25 15:50:23 +0200

    HOTFIX - serviceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,6 @@ self.addEventListener('push', event => {
     const options = {
       body: data.body,
       icon: '/logo192.png',
-      // badge: 'badge.png',
       data: {
         url: 'https://oneword.byst.re', // Dodaj URL Twojej aplikacji PWA
       },
@@ -22,19 +21,17 @@ self.addEventListener('notificationclick', function (event) {
   console.log('Notification click Received.');
   event.notification.close();
 
+  const urlToOpen = 'https://oneword.byst.re'; // Pełny URL aplikacji
+
   event.waitUntil(
-    clients
-      .matchAll({
-        type: 'window',
-      })
-      .then(function (clientList) {
-        for (var i = 0; i < clientList.length; i++) {
-          var client = clientList[i];
-          if (client.url === 'https://oneword.byst.re' && 'focus' in client) return client.focus();
-        }
-        if (clients.openWindow) {
-          return clients.openWindow('https://oneword.byst.re');
-        }
-      })
+    clients.matchAll({ type: 'window' }).then(function (clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url === urlToOpen && 'focus' in client) return client.focus();
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(urlToOpen);
+      }
+    })
   );
 });
