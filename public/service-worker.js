5d20ff60bad56b708553f315a29f4c4a0e9fdfa1
zofia janas <zoska91@wp.pl>
2024-08-25 15:10:22 +0200

    HOTFIX - notification for androis

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,10 +7,34 @@ self.addEventListener('push', event => {
       body: data.body,
       icon: '/logo192.png',
       // badge: 'badge.png',
+      data: {
+        url: 'https://oneword.byst.re', // Dodaj URL Twojej aplikacji PWA
+      },
     };
 
     event.waitUntil(self.registration.showNotification(data.title, options));
   } else {
     console.error('Push event data is missing');
   }
+});
+
+self.addEventListener('notificationclick', function (event) {
+  console.log('Notification click Received.');
+  event.notification.close();
+
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: 'window',
+      })
+      .then(function (clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url === 'https://oneword.byst.re' && 'focus' in client) return client.focus();
+        }
+        if (clients.openWindow) {
+          return clients.openWindow('https://oneword.byst.re');
+        }
+      })
+  );
 });
