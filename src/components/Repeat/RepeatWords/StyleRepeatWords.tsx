1fdb39dd2d5e17cb9489e42139d2125b379a4939
Paulina Golenia <70859448+Paullina26@users.noreply.github.com>
2024-09-07 13:27:02 +0200

    checking the correctness of the answers

--- src/components/Repeat/RepeatWords/StyleRepeatWords.tsx
+++ src/components/Repeat/RepeatWords/StyleRepeatWords.tsx
@@ -22,7 +22,7 @@ export const WrapperButton = styled.div`
 
 export const WrapperBaseWord = styled(WrapperLearning)``;
 
-export const InputStyle = styled(Input)`
+export const InputStyle = styled(Input)<{ $isCorrect: boolean | null }>`
   ${font_settings(2.2, 'normal', 400)}
   width: 100%;
   display: flex;
@@ -34,4 +34,12 @@ export const InputStyle = styled(Input)`
   background-color: rgba(232, 232, 232, 0.75);
   margin-bottom: 10px;
   position: relative;
+  /* background-color: ${({ $isCorrect }) =>
+    $isCorrect === null
+      ? 'rgba(232, 232, 232, 0.75);'
+      : $isCorrect
+      ? 'rgba(204, 225, 198, 0.75);'
+      : 'rgba(232, 203, 203, 0.75);'}; */
+  border: 3px solid
+    ${({ $isCorrect }) => ($isCorrect === null ? 'none' : $isCorrect ? 'green' : 'red')};
 `;
